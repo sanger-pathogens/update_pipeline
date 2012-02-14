a6c5e59ccd41248d29a246acef4d1676bda96d36
andrewjpage <andrewjpage@gmail.com>
2012-02-14 15:33:10 +0000

    parallelise the update pipeline script

--- update_pipeline.pl
+++ update_pipeline.pl
@@ -17,16 +17,18 @@ use Getopt::Long;
 use VRTrack::VRTrack;
 use VertRes::Utils::VRTrackFactory;
 use Data::Dumper;
+use Parallel::ForkManager;
 
 use UpdatePipeline::UpdateAllMetaData;
 use UpdatePipeline::Studies;
 
-my ( $studyfile, $help, $number_of_files_to_return, $database );
+my ( $studyfile, $help, $number_of_files_to_return, $parallel_processes, $database );
 
 GetOptions(
     's|studies=s'              => \$studyfile,
     'd|database=s'             => \$database,
     'f|max_files_to_return=s'  => \$number_of_files_to_return,
+    'p|parallel_processes=s'   => \$parallel_processes,
     'h|help'                   => \$help,
 );
 
@@ -34,14 +36,17 @@ my $db = $database ;
 
 ( $studyfile &&  $db && !$help ) or die <<USAGE;
 Usage: $0   
-  -s|--studies   <study name or file of SequenceScape study names>
-  -d|--database <vrtrack database name>
+  -s|--studies             <file of study names>
+  -d|--database            <vrtrack database name>
   -f|--max_files_to_return <optional limit on the number of files to check, sorted in desc order by filename>
-  -h|--help      <this message>
+  -p|--parallel_processes  <optional number of processes to run in parallel, defaults to 1>
+  -h|--help                <this message>
 
 Update the tracking database from IRODs and the warehouse
 
 USAGE
+
+$parallel_processes ||= 1;
 
 my $vrtrack = VertRes::Utils::VRTrackFactory->instantiate(
     database => $db,
@@ -53,6 +58,16 @@ unless ($vrtrack) {
 }
 
 my @study_names = UpdatePipeline::Studies->new(filename => $studyfile)->study_names;
-my $update_pipeline = UpdatePipeline::UpdateAllMetaData->new(study_names => @study_names, _vrtrack => $vrtrack, number_of_files_to_return =>$number_of_files_to_return );
-$update_pipeline->update();
+
+my $pm = new Parallel::ForkManager($parallel_processes); 
+foreach my $study_name (@study_names) {
+  $pm->start and next; # do the fork
+  my @split_study_names;
+  push(@split_study_names, $study_name);
+  
+  my $update_pipeline = UpdatePipeline::UpdateAllMetaData->new(study_names => @split_study_names, _vrtrack => $vrtrack, number_of_files_to_return =>$number_of_files_to_return );
+  $update_pipeline->update();
+  $pm->finish; # do the exit in the child process
+}
+$pm->wait_all_children;
 
