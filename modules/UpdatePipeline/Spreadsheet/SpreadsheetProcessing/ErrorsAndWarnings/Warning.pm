93d3825ae378d0cf3f811abb8ffb29cc2e022016
Craig Porter <cp7@sanger.ac.uk>
2013-09-23 11:16:24 +0100

    default warning description

--- modules/UpdatePipeline/Spreadsheet/SpreadsheetProcessing/ErrorsAndWarnings/Warning.pm
+++ modules/UpdatePipeline/Spreadsheet/SpreadsheetProcessing/ErrorsAndWarnings/Warning.pm
@@ -4,6 +4,8 @@ use Moose;
 
 extends 'UpdatePipeline::Spreadsheet::SpreadsheetProcessing::ErrorsAndWarnings::Base';
 
+has 'description' => ( is => 'ro', isa => 'Str', default => 'warning' );
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 1;
