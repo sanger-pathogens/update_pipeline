da5e43ba2adca2c18c2d977e215ebc2e616a0f5a
andrewjpage <andrewjpage@gmail.com>
2014-11-05 09:43:29 +0000

    update ebi acc number

--- lib/Warehouse/LibraryAliquots.pm
+++ lib/Warehouse/LibraryAliquots.pm
@@ -26,11 +26,9 @@ sub _construct_query {
     my ($self) = @_;
 
     return
-'select study.internal_id, aliquots.sample_internal_id, aliquots.receptacle_internal_id, sample.name,sample.accession_number, sample.common_name, library_tubes.name,study.accession_number, sample.supplier_name from current_studies as study 
+'select sample.name,  sample.supplier_name from current_studies as study 
   left join current_aliquots as aliquots on aliquots.study_internal_id = study.internal_id
   left join current_samples as sample on sample.internal_id = aliquots.sample_internal_id
-  left join current_'
-      . $self->receptacle_type . 's as library_tubes on library_tubes.internal_id =  aliquots.receptacle_internal_id
   where study.name like "'
       . $self->study_name . '"  and aliquots.receptacle_type like "' . $self->receptacle_type . '" and sample.common_name is not NULL';
 }
@@ -43,15 +41,8 @@ sub _build_libraries_metadata {
     for my $library_aliquot (@$library_aliquots) {
         my $library_metadata_container = UpdatePipeline::LibraryMetaData->new(
             study_name              => $self->study_name,
-            study_ssid              => @$library_aliquot[0],
-            study_accession_number  => @$library_aliquot[7],
-            library_name            => @$library_aliquot[6],
-            library_ssid            => @$library_aliquot[2],
-            sample_ssid             => @$library_aliquot[1],
-            sample_name             => @$library_aliquot[3],
-            sample_accession_number => @$library_aliquot[4],
-            sample_common_name      => @$library_aliquot[5],
-            supplier_name           => @$library_aliquot[8],
+            sample_name             => @$library_aliquot[0],
+            supplier_name           => @$library_aliquot[1],
         );
         push( @libraries_metadata, $library_metadata_container );
     }
