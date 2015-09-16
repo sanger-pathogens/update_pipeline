a3549295bc28ff0cefccba7b509391a6f9886c67
andrewjpage <andrewjpage@gmail.com>
2015-09-16 12:16:19 +0100

    expose threads option for spreadsheet

--- bin/update_pipeline_from_spreadsheet.pl
+++ bin/update_pipeline_from_spreadsheet.pl
@@ -22,7 +22,7 @@ use VRTrack::VRTrack;
 use VertRes::Utils::VRTrackFactory;
 use UpdatePipeline::Spreadsheet;
 
-my ( $help, $verbose_output, $database, $update_if_changed, $files_to_add_directory,$pipeline_base_directory );
+my ( $help, $verbose_output, $database, $update_if_changed, $files_to_add_directory,$pipeline_base_directory,$threads );
 
 GetOptions(
     'd|database=s'              => \$database,
@@ -30,10 +30,12 @@ GetOptions(
     'f|files_to_add_directory=s'  => \$files_to_add_directory,
     'p|pipeline_base_directory=s' => \$pipeline_base_directory,
     'u|update_if_changed'       => \$update_if_changed,
+	't|threads'                 => \$threads,
     'h|help'                    => \$help,
 );
 
 my $spreadsheet_filename = $ARGV[0];
+
 
 ( ( -e $spreadsheet_filename)  && $database && !$help ) or die <<USAGE;
 Usage: $0 [options] spreadsheet.xls
@@ -42,6 +44,7 @@ Usage: $0 [options] spreadsheet.xls
   -f|--files_to_add_directory  <base directory containing files to add to the pipeline>
   -p|--pipeline_base_directory <required if -f provided, path to sequencing pipeline root directory>
   -u|--update_if_changed       <optionally delete lane & file entries, if metadata changes, for reimport>
+  -t|--threads                 <number of threads to use>
 
   -h|--help                    <this message>
 
@@ -55,6 +58,7 @@ USAGE
 
 $verbose_output     ||= 0;
 $update_if_changed  ||= 0;
+$threads            ||= 1;
 
 my $vrtrack = VertRes::Utils::VRTrackFactory->instantiate(database => $database, mode     => 'rw');
 unless ($vrtrack) { die "Can't connect to tracking database: $database \n";}
@@ -68,6 +72,7 @@ my $spreadsheet = UpdatePipeline::Spreadsheet->new(
   study_names          => [],
   files_base_directory => $files_to_add_directory,
   pipeline_base_directory => $pipeline_base_directory,
+  parallel_processes => $threads
 );
 $spreadsheet->update();
 $spreadsheet->import_sequencing_files_to_pipeline() if(defined($files_to_add_directory));
