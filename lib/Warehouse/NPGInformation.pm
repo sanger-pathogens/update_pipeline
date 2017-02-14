08550e029ff47e9f5432cd7c6e976b96f73bfbd5
andrewjpage <andrewjpage@gmail.com>
2017-02-14 14:21:15 +0000

    dont lookup most metadata from warehouse

--- lib/Warehouse/NPGInformation.pm
+++ lib/Warehouse/NPGInformation.pm
@@ -54,36 +54,8 @@ sub populate
   }
 
   $self->_split_file_name;
-  $self->_populate_from_npg_information_table;
   $self->_populate_manual_qc_from_npg_information_table;
   1;
-}
-
-sub _populate_from_npg_information_table
-{
-  my($self) = @_;
-  if(defined($self->_id_run) && defined($self->_position)  )
-  {
-    my $id_run = $self->_id_run;
-    my $position = $self->_position;
-    my $sql = qq[select study_id, sample_id, asset_id, asset_name, paired_read, run_complete from npg_information where id_run = "$id_run" AND position = $position limit 1;];
-    my $sth = $self->_dbh->prepare($sql);
-    $sth->execute;
-    my @study_warehouse_details  = $sth->fetchrow_array;
-    if(@study_warehouse_details > 0)
-    {
-      $self->file_meta_data->study_ssid($study_warehouse_details[0])   if(!defined($self->file_meta_data->study_ssid));
-      $self->file_meta_data->lane_is_paired_read($study_warehouse_details[4]) if(defined($study_warehouse_details[4]));
-	  $self->file_meta_data->run_date($study_warehouse_details[5]) if(defined($study_warehouse_details[5]));
-      
-      if(! ($self->file_meta_data->file_name_without_extension =~ /#/))
-      {
-        $self->file_meta_data->sample_ssid($study_warehouse_details[1])  if(!defined($self->file_meta_data->sample_ssid));
-        $self->file_meta_data->library_ssid($study_warehouse_details[2]) if(!defined($self->file_meta_data->library_ssid));
-        $self->file_meta_data->library_name($study_warehouse_details[3]) if(!defined($self->file_meta_data->library_name));
-      }
-    }
-  }
 }
 
 sub _populate_manual_qc_from_npg_information_table
