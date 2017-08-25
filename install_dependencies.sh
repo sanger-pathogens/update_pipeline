ea29af63478add6bba3d65fbb256a3a31b606f86
Sara Sjunnebo <ss44@sanger.ac.uk>
2017-08-25 13:55:50 +0100

    update install deps script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,12 +5,29 @@ set -e
 
 start_dir=$(pwd)
 
+VRCODEBASE_GIT_URL='https://github.com/sanger-pathogens/vr-codebase.git'
+
+# Make an install location
+if [ ! -d 'git_repos' ]; then
+  mkdir git_repos
+fi
+cd git_repos
+
+git clone $VRCODEBASE_GIT_URL
+
+#Add lovations to PERL5LIB
+VRCODEBASE_LIB=${start_dir}'/git_repos/vr-codebase/modules'
+
+export PERL5LIB=${VRCODEBASE_LIB}:$PERL5LIB
+
 cd $start_dir
 
 cpanm Dist::Zilla
 dzil authordeps --missing | cpanm
 dzil listdeps | cpanm
-#cpanm Template YAML::XS File::Slurp DBI DBD::mysql
+cpanm File::Slurp DBI XML::TreePP Bio::DB::EUtilities Parallel::ForkManager Spreadsheet::ParseExcel Test::MockObject
+
+#Template YAML::XS DBD::mysql
 
 set +eu
 set +x
